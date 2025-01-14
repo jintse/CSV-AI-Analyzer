4bd6bcab692e7bb25a30f3d91536e8f89694bf32
jintse <166783105+jintse@users.noreply.github.com>
2025-01-14 23:29:44 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,30 +8,30 @@ PROMPT_TEMPLATE = """
     For text-based answers, respond in the following format: 
     {"answer": "<your answer here>"} Example: {"answer": "The product ID with the highest order volume is 'MNWC3-067'"}
     
-    If the user requests a table, respond in the following format: 
-    {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+    If the user requests a table and a text-based answers, respond in the following format: 
+    {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...] }, "answer": "<your answer here>"}
     
-    If the user's request suits a bar chart, respond in the following format: 
-    {"bar": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}}
+    If the user's request suits a bar chart and a text-based answers, respond in the following format: 
+    {"bar": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}, "answer": "<your answer here>"}
     
-    If the user's request suits a line chart, respond in the following format: 
-    {"line": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}}
+    If the user's request suits a line chart and a text-based answers, respond in the following format: 
+    {"line": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}, "answer": "<your answer here>"}
     
-    If the user's request suits a scatter plot, respond in the following format: 
-    {"scatter": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}}
+    If the user's request suits a scatter plot and a text-based answers, respond in the following format: 
+    {"scatter": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}, "answer": "<your answer here>"}
     
     Note: Only three types of charts are supported: "bar", "line", and "scatter".
     
     Ensure all outputs are returned as JSON strings. Remember to enclose all strings in the "columns" list and the data list with double quotes.
     Example:
-    {"table": {"columns": ["Products", "Orders"], "data": [["32085Lip", 245], ["76439Eye", 178]]}}
+    {"table": {"columns": ["Products", "Orders"], "data": [["32085Lip", 245], ["76439Eye", 178]]}, "answer": "<your answer here>"}
     
     The user request you need to handle is as follows:
     """
 
 
 def dataframe_agent(openai_api_key, df, query):
-    model = ChatOpenAI(model="gpt-4-turbo", openai_api_key=openai_api_key, openai_api_base="https://api.aigc369.com/v1",
+    model = ChatOpenAI(model="gpt-4o", openai_api_key=openai_api_key, openai_api_base="https://api.aigc369.com/v1",
                        temperature=0)
     agent = create_pandas_dataframe_agent(llm=model, df=df,
                                           agent_executor_kwargs={"handle_parsing_errors": True},
@@ -46,3 +46,4 @@ def dataframe_agent(openai_api_key, df, query):
 
 #df = pd.read_csv("personal_data.csv")
 #print(dataframe_agent(openai_api_key=, df=df, query="make a list of engineers"))
+
